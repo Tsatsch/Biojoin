76043100363ded4040a5fec01f57de9ec917b887
Tsatsch <sshsmnv2000@gmail.com>
2022-05-03 21:12:02 +0900

    fixed cmdline update

--- operate_db.py
+++ operate_db.py
@@ -37,21 +37,21 @@ def pre_update(db_connection):
         else:
             break
 
-        if answer == 'a':  # TODO
-            print(f'Available cols: {list_cols(db_connection, table)} ')
-            print("Please pay attention to order of attribute and format: elem1 = value, elem2 = value, ...")
-            content = input("Please specify the new content in SQL format:  ")
-            condition = input("What do you want to modify? Please provide in SQL format: ")
-            update(db_connection=db_connection, table=table, new_content=content,
-                   condition=condition, type_of_update='mod')
-        elif answer == 'b':
-            print(f'Available cols: {list_cols(db_connection, table)} ')
-            print("Please pay attention to order of attribute and format: elem1, elem2, ...")
-            content = input("Please provide the new values in SQL format:  ")
-            update(db_connection=db_connection, table=table,
-                   new_content=content, type_of_update='add_r')
-        else:
-            quit()
+    if answer == 'a':
+        print(f'Available cols: {list_cols(db_connection, table)} ')
+        print("Please pay attention to order of attribute and format: elem1 = value, elem2 = value, ...")
+        condition = input("What do you want to modify? Please provide in SQL format: ")
+        content = input("Please specify the new content in SQL format:  ")
+        update(db_connection=db_connection, table=table, new_content=content,
+               condition=condition, type_of_update='mod')
+    elif answer == 'b':
+        print(f'Available cols: {list_cols(db_connection, table)} ')
+        print("Please pay attention to order of attribute and format: elem1, elem2, ...")
+        content = input("Please provide the new values in SQL format:  ")
+        update(db_connection=db_connection, table=table,
+               new_content=content, type_of_update='add_r')
+    else:
+        quit()
 
 
 def update(db_connection, table, new_content, type_of_update, condition=None):
